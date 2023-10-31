b16b7031c18721c9a04b9227a11931d81e863fa0
panchal aditya nileshbhai <adityajsn1299@gmail.com>
2023-10-31 22:33:59 +0530

    HMS

---
+++ app/src/main/java/com/example/hms/student_registration.java
@@ -0,0 +1,14 @@
+package com.example.hms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class student_registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_registration);
+    }
+}
